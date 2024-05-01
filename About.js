620da657f3ce873d9a44717ead73248fa1a53d25
jeekpark <jeekpark@student.42seoul.kr>
2024-05-01 19:55:31 +0900

    Feat: init setting

---
+++ About.js
@@ -0,0 +1,10 @@
+
+class About {
+    template() {
+      return `
+      <div>About Page</div>
+      <button class="moveToHomePageBtn">Go to Home Page</button>
+      `;
+    }
+  }
+  export default new About();
---
+++ Home.js
@@ -0,0 +1,11 @@
+// Home.js
+
+class Home {
+    template() {
+      return `
+      <div>Home Page</div>
+      <button class="moveToAboutPageBtn">Go to About Page</button>
+      `;
+    }
+  }
+  export default new Home();
