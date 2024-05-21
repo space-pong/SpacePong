9d3070828e6e2b808502f99d0522b7b1413a9de7
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-05-21 16:55:11 +0900

    뒤로가기 구현

--- SPA_test/js/app.js
+++ SPA_test/js/app.js
@@ -10,16 +10,22 @@ const routes = {
 };
 
 function init() {
-    // 기본적으로 로그인 페이지를 로드
-    renderPage(routes.login);
+  // 기본적으로 로그인 페이지를 로드
+  renderPage(routes.login);
 
-    // 페이지 전환을 위한 이벤트 설정
-    document.body.addEventListener('click', (e) => {
-        if (e.target.matches('[data-link]')) {
-            const page = e.target.getAttribute('data-link');
-            renderPage(routes[page]);
-        }
-    });
+  // 페이지 전환을 위한 이벤트 설정
+  document.body.addEventListener('click', (e) => {
+      if (e.target.matches('[data-link]')) {
+          const page = e.target.getAttribute('data-link');
+          renderPage(routes[page]);
+          history.pushState({ page }, '', page);
+      }
+  });
 }
+
+window.addEventListener('popstate', (event) => {
+  const page = event.state?.page || 'login';
+  renderPage(routes[page]);
+});
 
 window.addEventListener('DOMContentLoaded', init);
