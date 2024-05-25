f9486090c27a039d049d81fb6ec19a8de1fddca3
jeekpark <jeekpark@student.42seoul.kr>
2024-05-26 03:39:13 +0900

    Update: 2024-05-26

--- web/indexAI.js
+++ web/indexAI.js
@@ -7,15 +7,17 @@ document.getElementById('art').addEventListener('click', async function handler(
   try {
     document.getElementById('art').removeEventListener('click', handler);
 
+    // 1. 컨트롤러
     const dualSenseController = new DualSenseController(); // 듀얼센스 게임패드 컨트롤러
     await dualSenseController.init()
     dualSenseController.setSubKeyboard(37, 39, 38, 40, 32); // 보조 키보드 방향키 + 스페이스바
-
     const aiController = new AIController(); // AI로 작동되는 자동 컨트롤러
 
+    // 2. 게임
     const game = new PongGame();
     await game.init(dualSenseController, aiController, "Zerg", "Zerg", 'art');
     game.start();
+    
   } catch (error) {
     console.error(error);
   }
--- web/indexLocal.js
+++ web/indexLocal.js
@@ -1,24 +1,22 @@
-import { PongGameRenderer } from './Game/PongGameRenderer.js';
-import { PongGameLogic } from './Game/PongGameLogic.js';
-import { KeyboardController } from './Game/KeyboardController.js';
+import { PongGame } from "./Game/PongGame.js"
+import { KeyboardController } from './Game/Controller/KeyboardController.js';
 
 
 document.getElementById('art').addEventListener('click', async function handler() {
   try {
     document.getElementById('art').removeEventListener('click', handler);
+
+    // 1. 컨트롤러
     const keyboard1 = new KeyboardController(37, 39, 38, 40, 32); // 방향키 + 스페이스바
     const keyboard2 = new KeyboardController(65, 68, 87, 83, 70); // wasd + f
-    const pongGameLogic = new PongGameLogic(keyboard1, keyboard2);
-    const pongRenderer = new PongGameRenderer();
-    await pongRenderer.init('art', pongGameLogic, "Zerg", "Zerg");
-    pongRenderer.setTopView();
-    pongRenderer.start();
-    pongGameLogic.start();
+
+    // 2. 게임
+    const game = new PongGame();
+    await game.init(keyboard1, keyboard2, "Default", "Zerg", 'art');
+    game.renderer.setTopView();
+    game.start();
 
   } catch (error) {
     console.error(error);
   }
 });
-
-
-
