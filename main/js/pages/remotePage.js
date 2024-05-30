09aaa1c72cdae79b0f259d381eb30cdc613ee2ed
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-05-30 13:01:31 +0900

    remote page css 제작

--- main/js/pages/remotePage.js
+++ main/js/pages/remotePage.js
@@ -1,27 +1,23 @@
 export const remotePage = {
   html: `
   <div class="control-bar__remote">
-    <header="control-bar__remote__header>Unit</header>
+    <header class="control-bar__remote__header">Unit</header>
     <div class="control-bar__remote__unit-selector">
-      <label class="unit-option">
+      <label class="unit-selector__unit-option">
+        <span class=unit-selector__unit-name>42lien</span>
         <input type="radio" name="unit" value="42lien">
-        <span class="custom-radio"></span>
-        42lien
       </label>
-      <label class="unit-option">
+      <label class="unit-selector__unit-option">
+        <span class=unit-selector__unit-name>Zerg</span>
         <input type="radio" name="unit" value="Zerg">
-        <span class="custom-radio"></span>
-        Zerg
       </label>
-      <label class="unit-option">
+      <label class="unit-selector__unit-option">
+        <span class=unit-selector__unit-name>Terran</span>
         <input type="radio" name="unit" value="Terran">
-        <span class="custom-radio"></span>
-        Terran
       </label>
-      <label class="unit-option">
+      <label class="unit-selector__unit-option">
+        <span class=unit-selector__unit-name>Protoss</span>
         <input type="radio" name="unit" value="Protoss">
-        <span class="custom-radio"></span>
-        Protoss
       </label>
     </div>
     <div class="control-bar__remote__confirm">
