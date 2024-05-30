6d671310602d34a0936075d58ed4481fb71f55d9
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-05-30 20:27:27 +0900

    부트스트랩 이용해서 시계모양 스피너 제작

--- main/js/app.js
+++ main/js/app.js
@@ -22,9 +22,10 @@ const routes = {
 
 function init() {
   //테스트위한 방법
-  renderControlBar(loginPage);
+  //renderControlBar(loginPage);
   //renderControlBar(mainPage);
   //renderControlBar(remotePage);
+  renderControlBar(remoteMatchPage);
   //renderControlBar(localAIPage);
   //renderControlBar(localOneToOnePage);
   //renderControlBar(localTournamentPage);
--- main/js/pages/remoteMatchPage.js
+++ main/js/pages/remoteMatchPage.js
@@ -4,7 +4,9 @@ export const remoteMatchPage = {
     <div class="control-bar__remoteMatch__title">Finding match</div>
     <div class="control-bar__remoteMatch__type">1 vs 1</div>
     <div class="control-bar__remoteMatch__icon">
-      <img src="/main/assets/images/icon_timer.svg" alt="icon of AI">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
     </div>
     <div class="control-bar__remoteMatch__confirm">
       <button class="control-bar__remoteMatch__confirm__btn--cancel" data-link="remotePage">Cancel</button>
