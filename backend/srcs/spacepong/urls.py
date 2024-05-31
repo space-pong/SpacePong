57da8c4da11afe19f79f703b51074d7d34e57f52
migo <migo96@gmail.com>
2024-05-31 13:58:22 +0900

    Modify: save issue

--- backend/srcs/spacepong/urls.py
+++ backend/srcs/spacepong/urls.py
@@ -1,14 +1,6 @@
 from django.urls import path, include
-<<<<<<< HEAD
 from . import views
 
 urlpatterns = [
-=======
-from .views import HELLoAPI
-from . import views
-
-urlpatterns = [
-        path("hello/", HELLoAPI),
->>>>>>> 390bbfae75df8f255d8512756a932f5fad670586
         path("data/", views.DataAPI.as_view()),
 ]
