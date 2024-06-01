b4a8a3957d9cd2d1bdccb3b19a6cc492bb6e2800
thomamin <thomamin@hotmail.com>
2024-06-01 15:16:24 +0900

    feature: django localization 기능을 이용한 다국어 지원 설정

---
+++ frontend/srcs/js/utils/translate.js
@@ -0,0 +1,20 @@
+    
+async function loadTranslation(key) {
+    const response = await fetch(`translation/?key=${key}`,{
+      method: 'GET',
+    });
+    const translations = await response.json();
+    console.log(translations.key);
+    return translations.key;
+}
+
+export function translate(elementId, translationKey) {
+    loadTranslation(translationKey).then(translatedText => {
+        const element = document.getElementById(elementId);
+        if (element) {
+            element.textContent = translatedText;
+        }
+    });
+}
+
+
