ed2dc7dae29a00c0a29c7c6cf0c580b5a563ff41
jeekpark <jeekpark@student.42seoul.kr>
2024-06-03 09:01:08 +0900

    Feat: 데이터 속성을 통해 페이지 흐름 제어, CSS 수정
    - app.js 수정
      - renderControlBar에 넘기는 패러미터에 페이지 흐름에 대한 정보 추가
      - data-target이라는 속성으로 현재 어떤 페이지 흐름인지 확인
    - renderControlbar.js 수정
      - data-target에 대한 정보가 있을 경우 data-link를 동적으로 수정
      - 페이드인아웃 효과를 위한 코드 추가
    - unitSelectPage.js 추가
      - 종족을 선택하는 창
      - data-link, data-target을 통해 페이지 흐름을 제어
    - remotePage.js 삭제
      - 해당 페이지는 unitSelectPage와 같은 기능이었음
    - remoteMatchPage.js, localAiPage, mainPage수정
      - data-target을 통해 흐름 제어
    - basic.css 수정
      - 페이지를 전환할 시, 페이드인 페이드 아웃 효과 적용
    - unitSelectPage.css 추가
    - remotePage.css 삭제
      - 이유는 위와 같음
    - localAIPage.css 수정
      - UI 디자인 통일

--- frontend/srcs/js/pages/mainPage.js
+++ frontend/srcs/js/pages/mainPage.js
@@ -5,10 +5,10 @@ export const mainPage = {
     <img src="/frontend/srcs/assets/images/icon_controller.svg" alt="controller">
     <img src="/frontend/srcs/assets/images/icon_setting.svg" alt="setting">
   </div>
-  <div class="control-bar__mode--text" data-link="remotePage">Remote Play</div>
+  <div class="control-bar__mode--text" data-link="unitSelectPage" data-target="remoteMatchPage">Remote Play</div>
   <div class="control-bar__mode--text" data-link="localTournamentPage">Tournament </div>
   <div class="control-bar__mode--text" data-link="localOneToOnePage">1 vs 1</div>
-  <div class="control-bar__mode--text" data-link="localAIPage">Single Play </div>
+  <div class="control-bar__mode--text" data-link="unitSelectPage" data-target="localAIPage">AI</div>
   `,
   css: 'styles/css/mainPage.css'
 };
