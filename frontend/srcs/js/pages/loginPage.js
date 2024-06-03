f54725c33926b6c60c97c02c3f6f90c2bf20af0c
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-06-03 21:32:30 +0900

    Add: 로그인 버튼 누를시 auth42인증 페이지 연결

--- frontend/srcs/js/pages/loginPage.js
+++ frontend/srcs/js/pages/loginPage.js
@@ -2,7 +2,7 @@
 
 export const loginPage = {
   html: `
-  <button class="control-bar__login-btn" data-link="mainPage">Login</button>
+  <button class="control-bar__login-btn" data-link="auth42">Login</button>
   `,
   css: 'styles/css/loginPage.css'
 };
---
+++ frontend/srcs/js/utils/checkToken.js
@@ -0,0 +1,48 @@
+export async function fetchTokens() {
+  try {
+    // 현재 URL에서 코드 가져오기
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+
+    if (!code) {
+    throw new Error('No authorization code provided');
+    }
+
+    // 액세스 토큰 요청을 위한 fetch 요청
+    const response = await fetch(`auth42/callback/?code=${code}`, {
+  	method: 'GET',
+    });
+
+    if (!response.ok) {
+    throw new Error('Failed to fetch tokens');
+    }
+    // JSON 형식의 응답 데이터 파싱
+    const data = await response.json();
+
+    // 로컬 스토리지에 토큰 저장
+    localStorage.setItem('accessToken', data.access_token);
+    // 모드 선택 페이지 렌더링
+  } catch (error) {
+    console.error('Error fetching tokens:', error);
+    return null;
+  }
+}
+
+export async function checkaccess() {
+  let token = localStorage.getItem('accessToken');
+  if (token) {
+    const accessresponse = await fetch(`auth42/access?access_token=${token}}`, {
+    method: 'GET'
+    });
+    if (!accessresponse.ok) {
+    const errorData = await response.json();
+    console.error('Error verifying access token:', errorData.error);
+    return;
+    }
+    const responseData = await accessresponse.json();
+   if (!responseData.message) {
+    token = responseData.access_token;
+    localStorage.setItem('accessToken', responseData.access_token);
+    }
+  }
+}
