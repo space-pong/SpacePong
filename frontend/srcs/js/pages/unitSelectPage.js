be8648ef04e5500a3eddedae1b0ef3ff9502ac14
thomamin <thomamin@hotmail.com>
2024-06-11 16:35:09 +0900

    modify: call translate from .js pages ver2  by making getHtml() async

--- frontend/srcs/js/pages/unitSelectPage.js
+++ frontend/srcs/js/pages/unitSelectPage.js
@@ -1,35 +1,27 @@
 import globalState from "../globalState.js";
-import { translate } from "../utils/translate.js";
+import { loadTranslation } from "../utils/translate.js";
 
 export const unitSelectPage = {
-  getHtml() {
-    const html = `
-    <div id="unit" class="control-bar__title">Unit</div>
+  async getHtml() {
+    return `
+    <div class="control-bar__title">${await loadTranslation('Unit')}</div>
     <div class="control-bar__type">${globalState.currentAlias}</div>
     <div class="control-bar__unit-selector">
       <label class="unit-selector__unit-option">
-        <span id="default" class="unit-selector__unit-name">Loading...</span>
+        <span class="unit-selector__unit-name">${await loadTranslation('Default')}</span>
         <input type="radio" name="unit" value="Default" checked>
       </label>
       <label class="unit-selector__unit-option">
-        <span id="zerg" class="unit-selector__unit-name">Loading...</span>
+        <span class="unit-selector__unit-name">${await loadTranslation('Zerg')}</span>
         <input type="radio" name="unit" value="Zerg">
       </label>
     </div>
     <div class="control-bar__confirm">
-      <button href="" class="control-bar__confirm__btn--select" data-link>Select</button>
-      <button href="/" class="control-bar__confirm__btn--cancel" data-link>Cancel</button>
+      <button href="" class="control-bar__confirm__btn--select" data-link>${await loadTranslation('Select')}</button>
+      <button href="/" class="control-bar__confirm__btn--cancel" data-link>${await loadTranslation('Cancel')}</button>
     </div>
     `;
 
-    // After rendering HTML, trigger the translation
-    setTimeout(() => {
-      translate('unit', 'Unit');
-      translate('default', 'Default');
-      translate('zerg', 'Zerg');
-    }, 0);
-
-    return html;
   },
   css: 'styles/css/unitSelectPage.css',
 };
