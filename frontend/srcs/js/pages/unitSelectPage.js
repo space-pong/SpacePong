d811cf831f4aa8fd4ec0de3a597c5cfcd3e89fc0
thomamin <thomamin@hotmail.com>
2024-06-11 15:57:45 +0900

    modify: call translate from .js pages

--- frontend/srcs/js/pages/unitSelectPage.js
+++ frontend/srcs/js/pages/unitSelectPage.js
@@ -1,17 +1,18 @@
 import globalState from "../globalState.js";
+import { translate } from "../utils/translate.js";
 
 export const unitSelectPage = {
   getHtml() {
-    return `
-    <div class="control-bar__title">Unit</div>
+    const html = `
+    <div id="unit" class="control-bar__title">Unit</div>
     <div class="control-bar__type">${globalState.currentAlias}</div>
     <div class="control-bar__unit-selector">
       <label class="unit-selector__unit-option">
-        <span class=unit-selector__unit-name>Default</span>
+        <span id="default" class="unit-selector__unit-name">Loading...</span>
         <input type="radio" name="unit" value="Default" checked>
       </label>
       <label class="unit-selector__unit-option">
-        <span class=unit-selector__unit-name>Zerg</span>
+        <span id="zerg" class="unit-selector__unit-name">Loading...</span>
         <input type="radio" name="unit" value="Zerg">
       </label>
     </div>
@@ -20,6 +21,15 @@ export const unitSelectPage = {
       <button href="/" class="control-bar__confirm__btn--cancel" data-link>Cancel</button>
     </div>
     `;
+
+    // After rendering HTML, trigger the translation
+    setTimeout(() => {
+      translate('unit', 'Unit');
+      translate('default', 'Default');
+      translate('zerg', 'Zerg');
+    }, 0);
+
+    return html;
   },
-  css: 'styles/css/unitSelectPage.css'
+  css: 'styles/css/unitSelectPage.css',
 };
