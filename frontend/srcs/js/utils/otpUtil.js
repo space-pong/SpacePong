656074fb43361aacc5bfdde5b85c4bc2eb914612
Eonjoo Oh <eoh@cx2r3s7.42seoul.kr>
2024-06-18 15:41:20 +0900

    Feat: OTP 기능 추가 - 42 intra에 등록된 mail로 otp번호 받기 - otp 번호 입력 시 유효성 검사 : 유효할 시 인증 완료 후 main으로 이동, 유효하지 않을 시 경고창띄움

--- frontend/srcs/js/utils/otpUtil.js
+++ frontend/srcs/js/utils/otpUtil.js
@@ -3,37 +3,45 @@ import globalState from '../globalState.js';
 export function otpUtil() {
     document.querySelector('#otpInput').focus();
     document.querySelector('#otpInput').onkeyup = function(e) {
+
         if (e.key === 'Enter') {
             document.querySelector('#otpSubmit').click();
         }
     };
 
     document.querySelector('#otpSubmit').onclick = async function(e) {
-        const OTP_num = document.querySelector('#otpInput').value;
-        const token = localStorage.getItem('accessToken');
-        try {
-            const response = await fetch(`twofactor/mail/?OTP=${OTP_num}`, {
-                method: 'GET',
-                headers: {
-                    "Content-Type": "application/json",
-                    'Authorization': 'Bearer ' + token
-                },
-            });
+        const OTP_num = document.querySelector('#otpInput').value.trim();
+        if (OTP_num)
+        {
+            console.log("I have otp num!");
+            const token = localStorage.getItem('accessToken');
+            try {
+                const response = await fetch(`twofactor/mail/?OTP=${OTP_num}`, {
+                    method: 'GET',
+                    headers: {
+                        "Content-Type": "application/json",
+                        'Authorization': 'Bearer ' + token
+                    },
+                });
 
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                const data = await response.json();
+                console.log("data: " , data);
+                console.log("data.message: ", data.message);
+                if (data === "OTP OK") {
+                    globalState.otp == true;
+                    window.location.pathname = '/index.html';
+                } else {
+                    alert("OTP authentication failed");
+                }
+            } catch (error) {
+                console.error('Error:', error);
             }
-            const data = await response.json();
-            console.log("data: " , data);
-            console.log("data.message: ", data.message);
-            if (data === "OTP OK") {
-                globalState.otp == true;
-                window.location.pathname = '/index.html';
-            } else {
-                alert("Fail: " + data.message);
-            }
-        } catch (error) {
-            console.error('Error:', error);
+        }
+        else {
+            alert("please enter otp number!");
         }
     };
 
